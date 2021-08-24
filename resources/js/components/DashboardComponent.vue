b90af783bd8f6b74f54386e0da2fbcbca1a5ce83
KumarShaikatBala <shaikat4015@diu.edu.bd>
2021-08-24 08:51:32 +0600

    folder vue pagination added

--- resources/js/components/DashboardComponent.vue
+++ resources/js/components/DashboardComponent.vue
@@ -6,10 +6,13 @@
                     <div class="card-header">Directory  Tree</div>
 
                     <div class="card-body">
-                        <ul v-for="folder in folders" :key="folder.id">
+                        <ul v-for="folder in folders.data" :key="folder.id">
                             <b class="text-info" v-if="folder.children">{{folder.folder}}</b>
 <li v-for="child in folder.children" :key="child.id">{{child.folder}}</li>
                         </ul>
+                    </div>
+                    <div class="card-footer">
+                        <pagination :data="folders" @pagination-change-page="getResults"></pagination>
                     </div>
                 </div>
             </div>
@@ -29,13 +32,20 @@
             loadFolders(){
                 this.$Progress.start();
                 axios.get('/api/folder/index')
-                    .then(({data})=>(this.folders=data.data))
+                    .then(({data})=>(this.folders=data.folders))
                     .catch(()=>{
                         this.$Progress.fail();
                     })
                 this.$Progress.finish();
             },
-
+            getResults(page = 1) {
+                axios.get('/api/folder/index?page=' + page)
+                    .then(({data})=>(this.folders=data.folders))
+                    .catch(()=>{
+                        this.$Progress.fail();
+                    })
+                this.$Progress.finish();
+            },
         },
         mounted() {
             this.loadFolders();
